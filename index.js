2020500d9ecf6f57f3aeb1e18b895f49be41fd2b
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-03-13 22:44:08 +0530

    Starting REST API Project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+
+const port = process.env.PORT || 1403;
+
+app.get('/',(req,res) => {
+    res.send('Hello from primary')
+})
+
+app.post('/students', (req, res) => {
+    res.send('Hello from secondary')
+})
+
+app.listen(port, () => {
+    console.log(`Server is up and Running on port ${port}`)
+})
